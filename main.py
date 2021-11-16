01a675044b01cada83e08437a57d9e91dd8d099f
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-11-17 01:01:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from typing import KeysView
+import pandas
+
+data = pandas.read_csv("D:\VS_Code Programs\Python\Alphabet split full-form Project\list_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+def generate_phoenetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the word please.")
+    else:
+        print(output_list)
+
+more_word = '1'
+while more_word == '1':
+    generate_phoenetic()
+    more_word = input("Want to search more? [0/1]: ")
+    
